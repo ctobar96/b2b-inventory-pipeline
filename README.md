551fb28ef3ae97ded1b9e8204cda1ee1dc3760c9
CTobar <cristiantobar96@gmail.com>
2026-08-11 19:49:12 -0400

    Agregar procesamiento y carga de datos en PostgreSQL en la función de limpieza, incluyendo manejo de credenciales desde el entorno.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # b2b-inventory-pipeline
+
+
+
+
+
+| Variable            | Significado                          | Tu valor           |
+| ------------------- | ------------------------------------ | ------------------ |
+| `POSTGRES_USER`     | Usuario de PostgreSQL                | `admin_datos`      |
+| `POSTGRES_PASSWORD` | Contraseña del usuario               | `superpassword123` |
+| `POSTGRES_DB`       | Base de datos inicial                | `b2b_analytics`    |
+| `POSTGRES_PORT`     | Puerto expuesto en tu máquina        | `5432`             |
+| `POSTGRES_HOST`     | Nombre del servidor dentro de Docker | `postgres`         |
+
+
+
+Para evitar esto y enfocarnos solo en la Fase 3, le diremos a Docker que levante exclusivamente el servicio de la base de datos.Sigue esta secuencia exacta en tu terminal:1.Inicia PostgreSQL:Levantar solo la base de datos.Primero, entra a la carpeta de Docker y levanta específicamente el servicio postgres en segundo plano (con la bandera -d):Bashcd docker
+docker-compose up -d postgres
+Nota: Verás que dice "Started b2b_postgres". Docker descargará la imagen (si no la tiene) y la encenderá en segundos.2.Vuelve a la raíz del proyecto:Asegurar la ruta de ejecución.Tu orquestador principal debe ejecutarse siempre desde la carpeta principal del proyecto, así que retrocede un nivel:Bashcd ..
+3.Ejecuta tu Orquestador E2E:La prueba de fuego.Ahora sí, con la base de datos esperando conexiones, lanza el pipeline completo:Bashpython -m src.main
+Si toda la configuración es correcta, verás en tu terminal cómo los datos se extraen (Fase 1), se aseguran en tu Data Lake de Amazon S3 (Fase 2) y, finalmente, se limpian y se inyectan en tu base de datos local en Docker (Fase 3), finalizando con el mensaje 🎉 PIPELINE E2E COMPLETADO EXITOSAMENTE.
--- src/transform/data_cleaner.py
+++ src/transform/data_cleaner.py
@@ -1,5 +1,6 @@
+import os
 import pandas as pd
-import sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 def procesar_y_cargar_db(ruta_archivo_local):
@@ -9,8 +10,34 @@ def procesar_y_cargar_db(ruta_archivo_local):
     print(f"Iniciando procesamiento del archivo '{ruta_archivo_local}'...")
 
     try:
-        pass
+        # 1. Extracción local
+        df = pd.read_csv(ruta_archivo_local)
 
+        # 2. Limpieza y transformación de datos
+        df['fecha_venta'] = pd.to_datetime(df['fecha_venta'], errors='coerce')  # Convertir a datetime
+
+        # Filtro de Calidad: eliminaos cualquier registro que tena un total de 0 o negativo
+        df =  df[df['total_clp'] > 0]
+
+        # 3. Conexión a la Base de Datos PostgreSQL (Docker)
+        # Leemos las credenciales desde tu .env de forma segura
+        pg_user = os.getenv("POSTGRES_USER")
+        pg_password = os.getenv("POSTGRES_PASSWORD")    
+        pg_db = os.getenv("POSTGRES_DB")
+        pg_port = os.getenv("POSTGRES_PORT", '5432')
+
+
+        # Formato: postgresql://usuario:contraseña@host:puerto/nombre_bd
+        # Estas credenciales hacen match exacto con tu docker-compose.yml
+        string_conexion = f"postgresql://{pg_user}:{pg_password}@{pg_port}/{pg_db}"
+        motor_db = create_engine(string_conexion)
+
+        # 4. Carga de datos (Load) a la tabla 'ventas_procesadas'
+        # if_exists='append' agrega las filas nuevas al final de la tabla si ya existe
+        df.to_sql(name='ventas_procesadas', con=motor_db, if_exists='append', index=False)
+        
+        print("Transformación exitosa. Datos cargados en PostgreSQL.")
+        return True
     
     except FileNotFoundError:
         print(f"Error: El archivo '{ruta_archivo_local}' no se encontró.")
